0851b49d69230792f73bb9ff0520592a11e561fb
Adriana Oliveira <adripdv@gmail.com>
2021-02-16 10:45:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# List items are enclosed in square brackets []
+# Lists are odered 
+# Lists are mutable 
+# List elements do not need to be unique
+# List can be of different data types
+
+''' List indexing'''
+
+
+''' How to slice lists in Python '''
+
+''' Add elements to a list '''
+
+''' Remove or Delete list items '''
+
+''' Python List Methods'''
